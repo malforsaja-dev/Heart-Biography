aeb8564be448ceb949ee47dadfdaab80f357fb63
n3ndor <107216827+n3ndor@users.noreply.github.com>
2024-08-23 13:00:54 -0400

    renamed the routes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,5 @@
 
-"use client"
-
-import React, { useEffect } from 'react';
-
 const About = () => {
-  console.log('About Page component is being rendered');
-
-  useEffect(() => {
-    console.log('About Page useEffect - component mounted');
-    return () => {
-      console.log('About Page useEffect - component unmounted');
-    };
-  }, []);
 
   return (
     <div className='bg-gray-100 h-screen w-screen'>
--- app/seiten-page/page.tsx
+++ app/seiten-page/page.tsx
@@ -1,18 +1,7 @@
-
-"use client"
-
-import React, { useEffect } from 'react';
 import WorkBench from '../components/WorkBench'
 
 const Seiten = () => {
-  console.log('Seiten-Page component is being rendered');
 
-  useEffect(() => {
-    console.log('Seiten-Page useEffect - component mounted');
-    return () => {
-      console.log('Seiten-Page useEffect - component unmounted');
-    };
-  }, []);
   return (
     <div className='bg-gray-100'>
       <WorkBench />
